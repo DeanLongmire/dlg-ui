8251ffcc5f81fdbfd1b77c5d28bf0fb6d7e5e3a4
Dean Longmire <deanlongmire100@gmail.com>
2025-10-04 16:27:22 -0400

    Make compatibility changes for Ember 6

--- testem.js
+++ testem.js
@@ -8,16 +8,21 @@ module.exports = {
   browser_start_timeout: 120,
   browser_args: {
     Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
+      mode: 'ci',
+      args: [
+        '--no-sandbox',
         '--headless',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900',
-      ].filter(Boolean),
+        '--disable-gpu',
+        '--disable-web-security',
+        '--disable-background-timer-throttling',
+        '--disable-renderer-backgrounding',
+        '--disable-backgrounding-occluded-windows',
+      ],
     },
   },
 };
