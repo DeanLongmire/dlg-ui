6bb8bbb0b3591e772fdcf4d13cb24f0cd9904d5b
Dean Longmire <deanlongmire100@gmail.com>
2025-10-12 17:28:40 -0400

    Update how css files are included

--- index.js
+++ index.js
@@ -2,4 +2,14 @@
 
 module.exports = {
   name: require('./package').name,
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    // Import individual CSS files for proper processing
+    app.import('addon/styles/dropdown.css');
+    app.import('addon/styles/fly-in.css');
+    app.import('addon/styles/navbar.css');
+    app.import('addon/styles/player-card.css');
+  },
 };
